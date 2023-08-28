a9a276d08e97050c3c5381a7c0a33d782bcd41a9
Aayam Adhikari <thisisleo7@gmail.com>
2023-08-28 19:13:15 +0545

    Initial Commit

---
+++ MyStore/MyStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStoreApp.swift
+//  MyStore
+//
+//  Created by Aayam Adhikari on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
